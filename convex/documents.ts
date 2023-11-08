84964adef155afcea356abd2c97abe7e2766824c
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-08 11:32:12 -0300

    remove and restore functions

--- convex/documents.ts
+++ convex/documents.ts
@@ -91,3 +91,91 @@ export const create = mutation({
     return document
   },
 })
+
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if (!identity) throw new Error('Usuário não autenticado')
+
+    const userId = identity.subject
+
+    const documents = await ctx.db
+      .query('documents')
+      .withIndex('by_user', (q) => q.eq('userId', userId))
+      .filter((q) => q.eq(q.field('isArchived'), true))
+      .order('desc')
+      .collect()
+
+    return documents
+  },
+})
+
+export const restore = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if (!identity) throw new Error('Usuário não autenticado')
+
+    const userId = identity.subject
+    const existingDoc = await ctx.db.get(args.id)
+
+    if (!existingDoc) throw new Error('Não encontrado')
+
+    if (existingDoc.userId !== userId) throw new Error('Não autorizado')
+
+    const recursiveRestore = async (documentId: Id<'documents'>) => {
+      const children = await ctx.db
+        .query('documents')
+        .withIndex('by_user_parent', (q) =>
+          q.eq('userId', userId).eq('parentDocument', documentId)
+        )
+        .collect()
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        })
+
+        await recursiveRestore(child._id)
+      }
+    }
+
+    const options: Partial<Doc<'documents'>> = { isArchived: false }
+
+    if (existingDoc.parentDocument) {
+      const parent = await ctx.db.get(existingDoc.parentDocument)
+      if (parent?.isArchived) {
+        options.parentDocument = undefined
+      }
+    }
+
+    await ctx.db.patch(args.id, options)
+
+    recursiveRestore(args.id)
+
+    return existingDoc
+  },
+})
+
+export const remove = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if (!identity) throw new Error('Usuário não autenticado')
+
+    const userId = identity.subject
+
+    const existingDoc = await ctx.db.get(args.id)
+
+    if (!existingDoc) throw new Error('Não encontrado')
+
+    if (existingDoc.userId !== userId) throw new Error('Não autorizado')
+
+    const document = await ctx.db.delete(args.id)
+
+    return document
+  },
+})
