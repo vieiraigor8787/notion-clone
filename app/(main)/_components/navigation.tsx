4487d4a75f368ca24ce24e13932e92cc1af01ba1
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-08 15:17:15 -0300

    trash box and undo function

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -26,6 +26,7 @@ import {
 import { UserItem } from './user-item'
 import { Item } from './item'
 import { DocumentList } from './document-list'
+import { TrashBox } from './trash-box'
 
 export const Navigation = () => {
   const pathname = usePathname()
@@ -158,7 +159,7 @@ export const Navigation = () => {
               side={isMobile ? 'bottom' : 'right'}
               className="p-0 w-72"
             >
-              <p>Arquivados</p>
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
---
+++ app/(main)/_components/trash-box.tsx
@@ -0,0 +1,112 @@
+'use client'
+
+import { useMutation, useQuery } from 'convex/react'
+import { useParams, useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { Search, Trash, Undo } from 'lucide-react'
+import { toast } from 'sonner'
+
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+
+import { Input } from '@/components/ui/input'
+import { Spinner } from '@/components/spinner'
+
+export const TrashBox = () => {
+  const router = useRouter()
+  const params = useParams()
+  const docs = useQuery(api.documents.getTrash)
+  const restore = useMutation(api.documents.restore)
+  const remove = useMutation(api.documents.remove)
+
+  const [search, setSearch] = useState('')
+
+  const filteredDocs = docs?.filter((doc) => {
+    return doc.title.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const onClick = (documentId: string) => {
+    router.push(`/documents/${documentId}`)
+  }
+
+  const onRestore = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    documentId: Id<'documents'>
+  ) => {
+    event.stopPropagation()
+
+    const promise = restore({ id: documentId })
+
+    toast.promise(promise, {
+      loading: 'Restaurando documento...',
+      success: 'Documento restaurado com sucesso',
+      error: 'Falha na restauração do documento',
+    })
+  }
+
+  const onRemove = (documentId: Id<'documents'>) => {
+    const promise = remove({ id: documentId })
+
+    toast.promise(promise, {
+      loading: 'Removendo documento...',
+      success: 'Documento removido com sucesso',
+      error: 'Falha na remoção do documento',
+    })
+
+    if (params.documentId === documentId) {
+      router.push('/documents')
+    }
+
+    if (docs === undefined) {
+      return (
+        <div className="h-full flex items-center justify-center p-4">
+          <Spinner size="lg" />
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className="text-sm">
+      <div className="flex items-center gap-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+          placeholder="Filtrar por título da página"
+        />
+      </div>
+      <div className="mt-2 px-1 pb-1">
+        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
+          Nenhum documento encontrado.
+        </p>
+        {filteredDocs?.map((filteredDoc) => (
+          <div
+            key={filteredDoc._id}
+            role="button"
+            onClick={() => onClick(filteredDoc._id)}
+            className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
+          >
+            <span className="truncate pl-2">{filteredDoc.title}</span>
+            <div className="flex items-center">
+              <div
+                onClick={(e) => onRestore(e, filteredDoc._id)}
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <div
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Trash className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
