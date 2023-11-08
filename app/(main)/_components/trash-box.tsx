f50ba0e74e8281a2761c8c83bf8ee9d7e517945c
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-08 16:31:12 -0300

    fix css

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -94,14 +94,14 @@ export const TrashBox = () => {
               <div
                 onClick={(e) => onRestore(e, filteredDoc._id)}
                 role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200"
+                className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
               >
                 <Undo className="h-4 w-4 text-muted-foreground" />
               </div>
               <ConfirmModal onConfirm={() => onRemove(filteredDoc._id)}>
                 <div
                   role="button"
-                  className="rounded-sm p-2 hover:bg-neutral-200"
+                  className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
                 >
                   <Trash className="h-4 w-4 text-muted-foreground" />
                 </div>
