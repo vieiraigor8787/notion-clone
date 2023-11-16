ba2a3ffc2ee4c846e78eff9be9c0daed5cb6ef74
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-16 10:59:52 -0300

    skeleton at title

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -20,7 +20,13 @@ export const Navbar = ({ onResetWidth, isCollapsed }: NavbarProps) => {
     documentId: params.documentId as Id<'documents'>,
   })
 
-  if (docId === undefined) return <p className="">Carregando...</p>
+  if (docId === undefined) {
+    return (
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full">
+        <Title.Skeleton />
+      </nav>
+    )
+  }
 
   if (docId === null) return null
 
--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -7,6 +7,7 @@ import { api } from '@/convex/_generated/api'
 import { Doc } from '@/convex/_generated/dataModel'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 
 interface TitleProps {
   initialData: Doc<'documents'>
@@ -72,4 +73,8 @@ export const Title = ({ initialData }: TitleProps) => {
       )}
     </div>
   )
+}
+
+Title.Skeleton = function TitleSkeleton() {
+  return <Skeleton className="h-9 w-16 text-muted-foreground" />
 }
