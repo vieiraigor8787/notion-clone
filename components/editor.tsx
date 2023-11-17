7b1d6309655a71fca1a3fdda6a44b6358a1eeecf
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-17 15:36:58 -0300

    block note js to text editable

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,36 @@
 'use client'
 
-export const Editor = () => {
-  return <div>editor</div>
+import { BlockNoteEditor, PartialBlock } from '@blocknote/core'
+import { BlockNoteView, useBlockNote } from '@blocknote/react'
+import '@blocknote/core/style.css'
+
+import { useTheme } from 'next-themes'
+
+interface EditorProps {
+  onChange: (value: string) => void
+  initialContent?: string
+  editable?: boolean
+}
+
+export const Editor = ({ initialContent, editable, onChange }: EditorProps) => {
+  const { resolvedTheme } = useTheme()
+
+  const editorNote: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2))
+    },
+  })
+
+  return (
+    <div className="">
+      <BlockNoteView
+        editor={editorNote}
+        theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
+      />
+    </div>
+  )
 }
