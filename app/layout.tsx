e34998dcefa114e04e2e3488986b3cd1def8467e
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-17 10:06:34 -0300

    using edge store - initial settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 import { ModalProvider } from '@/components/providers/modal-providers'
 
+import { EdgeStoreProvider } from '@/lib/edgestore'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -38,17 +40,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="jotion-theme-2"
-          >
-            <Toaster position="bottom-center" />
-            {children}
-            <ModalProvider />
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="jotion-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              {children}
+              <ModalProvider />
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
