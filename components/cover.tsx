b6dda0f66d602177f5e16c89a82534b96be66dda
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-11-17 15:10:46 -0300

    remove or replace image also from edge store

--- components/cover.tsx
+++ components/cover.tsx
@@ -11,6 +11,7 @@ import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 
 import { Button } from './ui/button'
+import { useEdgeStore } from '@/lib/edgestore'
 
 interface CoverProps {
   url?: string
@@ -21,8 +22,14 @@ export const Cover = ({ url, preview }: CoverProps) => {
   const params = useParams()
   const coverImage = useCoverImage()
   const removeImageCover = useMutation(api.documents.removeCoverImage)
+  const { edgestore } = useEdgeStore()
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      })
+    }
     removeImageCover({
       id: params.documentId as Id<'documents'>,
     })
@@ -40,7 +47,7 @@ export const Cover = ({ url, preview }: CoverProps) => {
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
